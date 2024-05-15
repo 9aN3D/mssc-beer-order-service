e048dca2446ec0694ae4181f963aabf99231a04e
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-05-15 21:44:44 +0200

    Add application-localmysql.properties

--- src/main/scripts/mysql-init.sql
+++ src/main/scripts/mysql-init.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS beerorderservice;
-DROP USER IF EXISTS `beerorderservice`@`%`;
+DROP USER IF EXISTS `beer_order_service`@`%`;
 CREATE DATABASE IF NOT EXISTS beerorderservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `beerorderservice`@`%` IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON `beerorderservice`.* TO `beerorderservice`@`%`;
+CREATE USER IF NOT EXISTS `beer_order_service`@`%` IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON `beerorderservice`.* TO `beer_order_service`@`%`;
 FLUSH PRIVILEGES;
